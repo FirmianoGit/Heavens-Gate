f352e223a439ef4f4562b6d2457fd9435487db74
Joao Vitor <firmianonetojoaovitor@gmail.com>
2024-04-05 13:17:48 -0300

    estudando

---
+++ api/src/application/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação - HeavensGate')
+    .setDescription(
+      'HeavensGate é uma aplicação para controle de presença e gerenciamento de membros de uma determinada congregação religiosa, fruto de um desafio proposto pela empresa TekSystem, a aplicação visa solucionar um problema proposto aos integrantes do grupo de desenvolvimento ',
+    )
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
